c08292b32ba00b9d9dc7b3e520882aace51adbde
Rajan Kasodariya <kasodariyarajan@gmail.com>
2015-09-01 15:11:54 +0530

    Update prolog 2.pl

--- Artificial Intelligence & Machine Learning/prolog 2.pl
+++ Artificial Intelligence & Machine Learning/prolog 2.pl
@@ -2,11 +2,15 @@
    using the concept of recursion and lists.(Hint: Use the 26 alphabets as a list)
 
 alphabets(X,[X|T]).
-alphabets(X,[H|T]):-
+alphabets(X,[H|T]):- 
 	alphabets(X,T).
 
-find:-
-	write('enter any value..'),nl,
-	read(Input),nl,
-	alphabets(Input,[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
+RUN : 
+alphabets(a,[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
+Ans : 
+true
+
+alphabets(aa,[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]).
+false.
+
 
